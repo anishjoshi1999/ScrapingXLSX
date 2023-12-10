0c75834c6875b2f22896bd7bab28360962d9a5e0
anishjoshi1999 <anishjoshi1999@gmail.com>
2023-12-10 09:31:54 +0545

    done

--- index.js
+++ index.js
@@ -8,32 +8,39 @@ const filePath = "./myfile.xlsx";
 // Read the XLSX file
 const workbook = XLSX.readFile(filePath);
 
-// Assuming your data is in the first sheet
-const sheetName = workbook.SheetNames[0];
-const sheet = workbook.Sheets[sheetName];
+(async () => {
+  async function processSheet(sheetIndex) {
+    let sheetName = workbook.SheetNames[sheetIndex];
+    const sheet = workbook.Sheets[sheetName];
 
-// Convert the sheet data to JSON
-const jsonData = XLSX.utils.sheet_to_json(sheet);
-// Process the data
-async function processData() {
-  for (let i = 0; i < jsonData.length; i++) {
-    const Name = jsonData[i].Name;
+    // Convert the sheet data to JSON
+    const jsonData = XLSX.utils.sheet_to_json(sheet);
 
-    const Website = jsonData[i].Website;
-    try {
-      const Email = await processWebsite(Website);
-      jsonData[i].Email = Email;
-      // Now you can do whatever you want with the data
-      console.log(`Email: ${Email}`);
-      // Update the XLSX file with the modified data
-      const updatedSheet = XLSX.utils.json_to_sheet(jsonData);
-      workbook.Sheets[sheetName] = updatedSheet;
-      XLSX.writeFile(workbook, filePath);
-      console.log(`${i + 1} Record updated successfully`);
-    } catch (error) {
-      console.error(`Error processing data for URL: ${Website}`, error);
+    for (let j = 0; j < jsonData.length; j++) {
+      const Name = jsonData[j].Name;
+      const Website = jsonData[j].Website;
+
+      try {
+        const Email = await processWebsite(Website);
+        jsonData[j].Email = Email;
+        // Now you can do whatever you want with the data
+        console.log(`Email: ${Email}`);
+        // Update the XLSX file with the modified data
+        const updatedSheet = XLSX.utils.json_to_sheet(jsonData);
+        workbook.Sheets[sheetName] = updatedSheet;
+        XLSX.writeFile(workbook, filePath);
+        console.log(`${j + 1} Record updated successfully`);
+      } catch (error) {
+        console.error(`Error processing data for URL: ${Website}`, error);
+        // If an error occurs, you may choose to handle it here
+        // For example, you could log the error, skip this record, or perform other actions
+      }
     }
+
+    console.log(`Scraping for sheet ${sheetName} successful`);
   }
-  console.log("Scraping Successfuly");
-}
-processData();
+
+  for (let i = 7; i < 8; i++) {
+    await processSheet(i);
+  }
+})();
